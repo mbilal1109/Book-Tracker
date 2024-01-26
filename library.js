107a597e3f8a63bd6634cd9a87d791d0601783f5
Mohammad Yousuf <mohammadbil1998@gmail.com>
2024-01-26 14:20:58 -0600

    Ability to Create Cards & Delete Button for Each Book

--- library.js
+++ library.js
@@ -4,11 +4,15 @@ const newBookBtn = document.querySelector("[data-new-book]");
 const cancelBtn = document.querySelector(".cancel");
 const addBookBtn = document.querySelector(".add-book");
 const form = document.querySelector(".form-container");
+const container = document.querySelector(".grid-container");
 
 const bName = document.querySelector("#book-name");
 const author = document.querySelector("#book-author");
 const pages = document.querySelector("#book-pages");
 const bStatus = document.querySelector("#book-status");
+
+let card;
+let delButton;
 
 function Book(name, author, pages, status) {
     this.name = name;
@@ -21,12 +25,35 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function displayBooks() {
-    console.log("Books")
+function displayBooks(book) {
+    createCard();
+    createDeleteButton();
+
+    let p1 = document.createElement("p");
+    p1.textContent = book.title;
+
+    let p2 = document.createElement("p");
+    p2.textContent = book.author;
+
+    let p3 = document.createElement("p");
+    p3.textContent = book.pages;
+
+    let p4 = document.createElement("p");
+    p4.textContent = book.status;
+
+    card.append(p1, p2, p3, p4, delButton);
+    container.appendChild(card);
 }
 
 function createCard() {
-    console.log("card")
+    card = document.createElement("div");
+    card.setAttribute("class", "book-info");
+}
+
+function createDeleteButton() {
+    delButton = document.createElement("button");
+    delButton.textContent = "Delete Book";
+    delButton.setAttribute("class", "delete-button");
 }
 
 addBookBtn.addEventListener("click", (event) => {
@@ -42,8 +69,11 @@ addBookBtn.addEventListener("click", (event) => {
     bookPages = pages.value;
     bookStatus = bStatus.value;
 
-    console.log(bookName);
+    const bookObject = new Book(bookName, bookAuthor, bookPages, bookStatus);
+    addBookToLibrary(bookObject);
     dialog.close();
+
+    displayBooks(bookObject);
 });
 
 newBookBtn.addEventListener("click", () => {
