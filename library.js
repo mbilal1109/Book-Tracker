af92a2a310c63d26efc38adc7b4c798e07998a22
Mohammad Yousuf <mohammadbil1998@gmail.com>
2024-01-26 13:48:11 -0600

    Ability to Save Book Data

--- library.js
+++ library.js
@@ -1,9 +1,14 @@
 const myLibrary = [];
-
-const dialog = document.querySelector("[data-dialog]");
+const dialog = document.querySelector("#new-book-dialog");
 const newBookBtn = document.querySelector("[data-new-book]");
-const cancelBtn = document.querySelector("[data-cancel]");
-const form = document.querySelector("[data-form]");
+const cancelBtn = document.querySelector(".cancel");
+const addBookBtn = document.querySelector(".add-book");
+const form = document.querySelector(".form-container");
+
+const bName = document.querySelector("#book-name");
+const author = document.querySelector("#book-author");
+const pages = document.querySelector("#book-pages");
+const bStatus = document.querySelector("#book-status");
 
 function Book(name, author, pages, status) {
     this.name = name;
@@ -16,10 +21,36 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function displayBooks() {
+    console.log("Books")
+}
+
+function createCard() {
+    console.log("card")
+}
+
+addBookBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let bookName;
+    let bookAuthor;
+    let bookPages;
+    let bookStatus;
+    
+    bookName = bName.value;
+    bookAuthor = author.value;
+    bookPages = pages.value;
+    bookStatus = bStatus.value;
+
+    console.log(bookName);
+    dialog.close();
+});
+
 newBookBtn.addEventListener("click", () => {
     dialog.showModal();
 });
 
 cancelBtn.addEventListener("click", () => {
+    console.log("Hello")
     dialog.close();
 });
